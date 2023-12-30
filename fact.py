0e8fe49e221cb6db5a82dbee1f2f88103b323c7b
rubyazz <yersultan.abduvalov@beksar.kz>
2023-12-30 18:54:00 +0600

    learning fastapi

--- fact.py
+++ fact.py
@@ -1,25 +1,38 @@
-def fact(num):
-    if num == 0 or num == 1:
-        return 1
-    else:
-        return num * fact(num - 1)
+# def fact(num):
+#     if num == 0 or num == 1:
+#         return 1
+#     else:
+#         return num * fact(num - 1)
 
 
-print(fact(50))
+# print(fact(50))
 
 
-def pali(w, first, last):
-    if w[first] != w[last]:
-        return False
-    elif last <= first:
-        return True
-    else:
-        return pali(w, first + 1, last - 1)
+# def pali(w, first, last):
+#     if w[first] != w[last]:
+#         return False
+#     elif last <= first:
+#         return True
+#     else:
+#         return pali(w, first + 1, last - 1)
 
 
-word = input()
+# word = input()
 
-first = 0
-last = len(word) - 1
+# first = 0
+# last = len(word) - 1
 
-print(pali(word, first, last))
+# print(pali(word, first, last))
+
+
+# def buble_sort(list):
+#     for i in range(len(arr)):
+#         for j in range(i):
+#             if arr[i] < arr[j]:
+#                 arr[i], arr[j] = arr[j], arr[i]
+#     return arr
+
+
+# arr = [8888, -451, 1, 45, 2, 0, 54, 4545, -45, 62, 15]
+
+# print(buble_sort(arr))
