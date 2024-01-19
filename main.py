703cbd88702f8f3d5fb514ff5611916f10d94fb7
rubyazz <yersultan.abduvalov@beksar.kz>
2024-01-20 02:21:43 +0600

    database creation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # from fastapi import Depends, FastAPI, HTTPException, Request, Response
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI(title="Microservice App")
 
@@ -8,16 +7,3 @@ app = FastAPI(title="Microservice App")
 @app.get("/users/")
 async def get_hello():
     return "Hello world!"
-
-
-@app.post("/users/")
-async def init_users():
-    return "initiation users"
-
-
-class Users(BaseModel):
-    pass
-
-
-class Role(BaseModel):
-    pass
