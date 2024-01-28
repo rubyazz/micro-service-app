3714630a35a28b1801c6103d2498009c02005957
rubyazz <yersultan.abduvalov@beksar.kz>
2024-01-28 14:23:44 +0600

    operations

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
 from sqlalchemy import (
     JSON,
     TIMESTAMP,
@@ -11,6 +12,8 @@ from sqlalchemy import (
     String,
     Table,
 )
+
+from database import Base
 
 metadata = MetaData()
 
@@ -35,3 +38,15 @@ user = Table(
     Column("is_superuser", Boolean, default=False, nullable=False),
     Column("is_verified", Boolean, default=False, nullable=False),
 )
+
+
+class User(SQLAlchemyBaseUserTable[int], Base):
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    username = Column(String, nullable=False)
+    registered_at = Column(TIMESTAMP, default=datetime.utcnow)
+    role_id = Column(Integer, ForeignKey(role.c.id))
+    hashed_password: str = Column(String(length=1024), nullable=False)
+    is_active: bool = Column(Boolean, default=True, nullable=False)
+    is_superuser: bool = Column(Boolean, default=False, nullable=False)
+    is_verified: bool = Column(Boolean, default=False, nullable=False)
---
+++ src/main.py
@@ -0,0 +1,23 @@
+from base_config import auth_backend, fastapi_users
+from fastapi import FastAPI
+from schemas import UserCreate, UserRead
+
+from operations.router import router as router_operation
+
+app = FastAPI(title="Trading App")
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth",
+    tags=["Auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["Auth"],
+)
+
+app.include_router(router_operation)
+
+# 6:27
